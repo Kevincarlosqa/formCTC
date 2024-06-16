3e5347eaf37a9ff6cbe64f9799a537a0f81f391c
Kevincarlosqa <kevincarlosqa@gmail.com>
2024-06-16 17:30:19 -0500

    Change server

--- vite.config.js
+++ vite.config.js
@@ -1,29 +1,29 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 import { createHtmlPlugin } from 'vite-plugin-html';
 
+
 export default defineConfig({
-  plugins: [
-    react(),
-    // Descomenta esto si necesitas agregar la política de seguridad de contenido
+  plugins: [react()
+    , 
     createHtmlPlugin({
-      inject: {
-        injectData: {
-          head: `
-            <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
-          `,
-        },
+    inject: {
+      injectData: {
+        head: `
+          <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
+        `,
       },
-    }),
-  ],
-  // server: {
-  //   port: 3000,
-  //   proxy: {
-  //     '/db': {
-  //       target: 'http://sheets.devcrackthecode.net/api/v1', // URL base de tu servidor NocoDB
-  //       changeOrigin: true,
-  //       rewrite: (path) => path.replace(/^\/db/, '') // Ajusta según la estructura de tu API
-  //     }
-  //   }
-  // }
-});
+    },
+  }),
+],
+server: {
+  proxy: {
+    '/api': {
+      target: 'http://sheets.devcrackthecode.net', // URL base de tu servidor NocoDB
+      changeOrigin: true,
+      rewrite: (path) => path.replace(/^\/api/, '/api/v1/db/data/v1') // Ajusta según la estructura de tu API
+    }
+  },
+  port: 3000
+}
+})
